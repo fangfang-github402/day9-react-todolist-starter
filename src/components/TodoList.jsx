68207f0d62e7f7812b1aa8af0e896c00bac6b0b0
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-28 19:05:56 +0800

    feat:add todo list css

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,11 @@
 import TodoGroup from "./TodoGroup";
 import TodoGenerator from "./TodoGenerator";
+import './TodoList.css'
 
 const TodoList = () => {
     return (
-        <div>
-            <div>Todo List</div>
+        <div className={"todo-list"}>
+            <div className={"title"}>Todo List</div>
             <TodoGroup/>
             <TodoGenerator/>
         </div>

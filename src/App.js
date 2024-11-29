303fa3fc2e5ec88c0870c4ca1e1f9a4a93eaa691
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-29 10:26:37 +0800

    feat:add 404NotFound route and set homepage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import {createContext, useReducer} from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
 import {initialState, todoReducer} from "./context/todoReducer";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import NotFound from "./components/NotFound";
 
 export const TodoContext = createContext();
 
@@ -10,15 +11,16 @@ function App() {
     const [state, dispatch] = useReducer(todoReducer, initialState);
 
     return (<div className="App">
-            <TodoContext.Provider value={{state, dispatch}}>
-                <Router>
-                    <Routes>
-                        <Route path={"/todo-list"} element={<TodoList/>}>
-                        </Route>
-                    </Routes>
-                </Router>
-            </TodoContext.Provider>
-        </div>);
+        <TodoContext.Provider value={{state, dispatch}}>
+            <Router>
+                <Routes>
+                    <Route path={"/"} element={<Navigate to="/todo-list"/>}></Route>
+                    <Route path={"/todo-list"} element={<TodoList/>}></Route>
+                    <Route path={"*"} element={<NotFound/>}></Route>
+                </Routes>
+            </Router>
+        </TodoContext.Provider>
+    </div>);
 }
 
 export default App;

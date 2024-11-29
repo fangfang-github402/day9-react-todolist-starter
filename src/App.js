80f76c39f6c95f45b3d115ec893aa055d45a20e9
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-29 10:11:58 +0800

    feat:add todo-list route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { createContext, useReducer } from "react";
+import {createContext, useReducer} from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
-import { initialState, todoReducer } from "./context/todoReducer";
+import {initialState, todoReducer} from "./context/todoReducer";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 export const TodoContext = createContext();
 
 function App() {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+    const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  return (
-    <div className="App">
-      <TodoContext.Provider value={{ state, dispatch }}>
-        <TodoList/>
-      </TodoContext.Provider>
-    </div>
-  );
+    return (<div className="App">
+            <TodoContext.Provider value={{state, dispatch}}>
+                <Router>
+                    <Routes>
+                        <Route path={"/todo-list"} element={<TodoList/>}>
+                        </Route>
+                    </Routes>
+                </Router>
+            </TodoContext.Provider>
+        </div>);
 }
 
 export default App;
